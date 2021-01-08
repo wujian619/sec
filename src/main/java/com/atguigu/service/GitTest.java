f83127046f7ceed0ea8e1c8f708ec4d9c9f3a7c8
wujian <wujian@163.com>
2021-01-08 22:14:09 +0800

    gittest

--- src/main/java/com/atguigu/service/GitTest.java
+++ src/main/java/com/atguigu/service/GitTest.java
@@ -1,4 +1,8 @@
 package com.atguigu.service;
 
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("never giveup");
+        System.out.println("gogogogo");
+    }
 }
